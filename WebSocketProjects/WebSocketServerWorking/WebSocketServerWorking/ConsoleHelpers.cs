e24a5be32cff027d9d38e0611761140a7b092eb6
dehoft <edvinas.deksnys@gmail.com>
2019-09-18 19:48:41 +0300

    transfered code to different .cs files

---
+++ WebSocketProjects/WebSocketServerWorking/WebSocketServerWorking/ConsoleHelpers.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+using System.Drawing;
+using Newtonsoft.Json.Linq;
+
+namespace WebSocketServerWorking
+{
+    public class ConsoleHelpers
+    {
+        public static void SetInitialWindowSize()
+        {
+            Console.SetWindowSize(Console.LargestWindowWidth / 100 * 75, Console.LargestWindowHeight / 2);
+        }
+
+        public static void PrintLogo()
+        {
+            Console.WriteLine("*************************************************************************************************************************************************");
+            Console.WriteLine("*       :::::::::           :::        ::::::::       ::::::::::       ::::::::::       :::::::::::       ::::::::::       :::        ::::::::: *");
+            Console.WriteLine("*      :+:    :+:        :+: :+:     :+:    :+:      :+:              :+:                  :+:           :+:              :+:        :+:    :+: *");
+            Console.WriteLine("*     +:+    +:+       +:+   +:+    +:+             +:+              +:+                  +:+           +:+              +:+        +:+    +:+  *");
+            Console.WriteLine("*    +#++:++#:       +#++:++#++:   +#+             +#++:++#         :#::+::#             +#+           +#++:++#         +#+        +#+    +:+   *");
+            Console.WriteLine("*   +#+    +#+      +#+     +#+   +#+             +#+              +#+                  +#+           +#+              +#+        +#+    +#+    *");
+            Console.WriteLine("*  #+#    #+#      #+#     #+#   #+#    #+#      #+#              #+#                  #+#           #+#              #+#        #+#    #+#     *");
+            Console.WriteLine("* ###    ###      ###     ###    ########       ##########       ###              ###########       ##########       ########## #########       *");
+            Console.WriteLine("*************************************************************************************************************************************************");
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        public static void PrintInitialServerInfo(System.Net.IPAddress ip, int port)
+        {
+            Console.WriteLine("*****Websocket server started*****");
+            Console.WriteLine("IP: {0}", ip);
+            Console.WriteLine("PORT: {0}", port);
+            Console.WriteLine("**********************************");
+        }
+    }
+
+}
---
+++ WebSocketProjects/WebSocketServerWorking/WebSocketServerWorking/Player.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+using System.Drawing;
+using Newtonsoft.Json.Linq;
+
+namespace WebSocketServerWorking
+{
+    public class Player
+    {
+        public Point location { get; private set; } // user location should be changed via method after all checks if distance speed ok
+        public string ID; //secret sesssion id
+        public string username; //player username
+        public int id; // public player id
+
+        //Settings
+        Color color;
+
+        public Player(string id, int pubid, string username, Color color, Point pos)
+        {
+            ID = id;
+            this.id = pubid;
+            this.username = username;
+            location = pos;
+            this.color = color;
+        }
+
+        /*
+         * FUNCTION: Gets sensitive player data - can be sent only to that user
+         * RETURN: 
+         */
+        public JObject GetMyStats()
+        {
+            JObject data = new JObject();
+            data["id"] = id;
+            data["username"] = username;
+            data["color"] = color.Name;
+            data["location"] = new JObject();
+            data["location"]["X"] = location.X;
+            data["location"]["Y"] = location.Y;
+            return data;
+        }
+
+        /*
+         *  FUNCTION: Gets public player data - can be sent to other user
+         *  RETURN: 
+         */
+        public JObject GetMyPublicStats()
+        {
+            JObject data = new JObject();
+            data["username"] = username;
+            data["location"] = new JObject();
+            data["color"] = color.Name;
+            data["location"]["X"] = location.X;
+            data["location"]["Y"] = location.Y;
+            return data;
+        }
+    }
+}
