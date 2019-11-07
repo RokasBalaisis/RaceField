5a44883141160545c5a9ee2de070297df9c09bab
Margiris <34870816+Margiris@users.noreply.github.com>
2019-11-07 20:45:58 +0200

    forgot to commit one file

--- WebSocketProjects/WebSocketServerWorking/WebSocketServerWorking/Collectables/CollectableFactory.cs
+++ WebSocketProjects/WebSocketServerWorking/WebSocketServerWorking/Collectables/CollectableFactory.cs
@@ -1,26 +1,50 @@
-﻿namespace WebSocketServerWorking.Collectables
+﻿using System;
+
+namespace WebSocketServerWorking.Collectables
 {
     class CollectableFactory : Factory
     {
-        private Bomb _bomb;
-        private SpeedBoost _speedBoost;
+        private readonly Bomb _bomb;
+        private readonly SpeedBoost _speedBoost;
 
         public CollectableFactory()
         {
-            _bomb = new Bomb();
-            _speedBoost = new SpeedBoost();
+            _bomb = (Bomb) GetCollectable(Collectable.Type.Bomb);
+            _speedBoost = (SpeedBoost) GetCollectable(Collectable.Type.SpeedBoost);
         }
 
         public override Collectable GetCollectable(Collectable.Type type)
         {
+            return GetCollectable(type, Bomb.Variant.Medium);
+        }
+
+        public Collectable GetCollectable(Collectable.Type type, Bomb.Variant bombVariant)
+        {
             switch (type)
             {
                 case Collectable.Type.Bomb:
-                    return (Bomb)_bomb.Clone();
+                    try
+                    {
+                        var temp = (Bomb) _bomb.Clone();
+                        temp.ChangeVariant(bombVariant);
+                        return  temp;
+                    }
+                    catch (NullReferenceException)
+                    {
+                        return new Bomb(bombVariant);
+                    }
                 case Collectable.Type.SpeedBoost:
-                    return (SpeedBoost)_speedBoost.Clone();
+                    try
+                    {
+                        var temp = (SpeedBoost) _speedBoost.Clone();
+                        return  temp;
+                    }
+                    catch (NullReferenceException)
+                    {
+                        return new SpeedBoost();
+                    }
                 default:
-                    return null;
+                    throw new ArgumentOutOfRangeException(nameof(type), type, "Unknown type of collectable");
             }
         }
     }
