911e8be9292106eb4adce3c83cdbd2aefe649401
Margiris <34870816+Margiris@users.noreply.github.com>
2019-11-07 21:34:52 +0200

    Update keep-alive.bat

--- WebSocketProjects/WebSocketServerWorking/keep-alive.bat
+++ WebSocketProjects/WebSocketServerWorking/keep-alive.bat
@@ -1,6 +1,5 @@
 :loop
 tasklist /FI "IMAGENAME eq WebSocketServerWorking.exe" 2>NUL | find /I /N "WebSocketServerWorking.exe" >NUL
-if "%ERRORLEVEL%" NEQ "0" START "" ".\WebSocketServerWorking\bin\Release\WebSocketServerWorking.exe" "start"
-echo will loop
+if "%ERRORLEVEL%" NEQ "0" START "" /wait ".\WebSocketServerWorking\bin\Release\WebSocketServerWorking.exe" "start"
 
-REM goto loop
+goto loop
