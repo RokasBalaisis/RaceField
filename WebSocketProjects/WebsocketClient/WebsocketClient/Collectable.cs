db25799e3a7b7bcfa41f9f8d0527795fd6f117ec
RokasBalaisis <rokas.balaisis@ktu.edu>
2019-11-07 20:59:15 +0200

    Added missing related files

---
+++ WebSocketProjects/WebsocketClient/WebsocketClient/Collectable.cs
@@ -0,0 +1,20 @@
+﻿namespace WebsocketClient
+{
+    public abstract class Collectable
+    {
+        public enum Type
+        {
+            Bomb,
+            SpeedBoost
+        }
+
+        public int durationMilliseconds; // duration in milliseconds of the collectable item effect
+        public double strength; // strength could be bigger for better performing players, decrease over time, etc.
+
+        // make collectable effective
+        public abstract void ApplyEffect();
+
+        // make collectable ineffective
+        public abstract void RemoveEffect();
+    }
+}
