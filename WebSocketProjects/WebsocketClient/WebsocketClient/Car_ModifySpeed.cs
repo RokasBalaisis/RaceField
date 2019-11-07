6eabdfecf442fdc1185d695be495971fa170b204
RokasBalaisis <rokas.balaisis@ktu.edu>
2019-11-07 14:31:27 +0200

    Commander design pattern for Car value modifications created

---
+++ WebSocketProjects/WebsocketClient/WebsocketClient/Car_ModifySpeed.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebsocketClient
+{
+    class Car_ModifySpeed
+    {
+        private readonly List<ICommand> _commands;
+        private ICommand _command;
+
+        public Car_ModifySpeed()
+        {
+            _commands = new List<ICommand>();
+        }
+
+        public void SetCommand(ICommand command)
+        {
+            _command = command;
+        }
+
+        public void Invoke()
+        {
+            _commands.Add(_command);
+            _command.ExecuteAction();
+        }
+
+        public void UndoActions()
+        {
+            foreach(var command in Enumerable.Reverse(_commands))
+            {
+                command.UndoAction();
+            }
+        }
+    }
+}
