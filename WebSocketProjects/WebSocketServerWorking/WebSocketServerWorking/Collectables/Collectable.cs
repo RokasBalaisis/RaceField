b4b425286220df48bb9ffcac306cb58e2ff69d24
Margiris <34870816+Margiris@users.noreply.github.com>
2019-11-22 10:01:59 +0200

    add Template Method pattern

--- WebSocketProjects/WebSocketServerWorking/WebSocketServerWorking/Collectables/Collectable.cs
+++ WebSocketProjects/WebSocketServerWorking/WebSocketServerWorking/Collectables/Collectable.cs
@@ -1,4 +1,6 @@
-﻿namespace WebSocketServerWorking.Collectables
+﻿using System.Threading;
+
+namespace WebSocketServerWorking.Collectables
 {
     public abstract class Collectable
     {
@@ -12,10 +14,28 @@
         public double effectStrength { get; protected set; } // effectStrength could be bigger for better performing players, decrease over time, etc.
         protected string spriteName; // name of the sprite resource used to represent collectable on the map
 
+        // draw animation 
+        public abstract void Animate();
+
         // make collectable effective
         public abstract void ApplyEffect();
 
         // make collectable ineffective
         public abstract void RemoveEffect();
+
+        // start new thread for each pickup to not block main thread while waiting
+        public void PickUp()
+        {
+            new Thread(PickUpAndWaitAsync).Start();
+        }
+
+        // use picked up collectable item
+        private void PickUpAndWaitAsync()
+        {
+            ApplyEffect();
+            Animate();
+            Thread.Sleep(duration);
+            RemoveEffect();
+        }
     }
 }
